dc3376fc46f9bd37a5d757091bf4f8acccca35cd
1985Viacheslav <viacheslav1985@mail.ru>
2023-08-15 20:15:16 +0300

    Initial commit

---
+++ les1.py
@@ -0,0 +1,59 @@
+# Типы данных
+# Числа: int (integer - целочисленный), float (floationg - плавающий, числа с плавющей запятой, дробные)
+# Текст: str (string - строка)
+# Логические: bool (boolean - логический)
+
+var = 123
+var = '123'
+var = "123"
+
+var = '123'
+# print(var + '3')
+# print(var)
+
+var = 120
+# print(var + 1)
+
+# PEP8 - Стиль написания кода
+# Linter - помогает писать с соответсвующим стилем код
+
+# Базовые операции над числами
+# + 
+# - 
+# * 
+# / 
+# // - целочсиленное деление
+# %  - остаток от деления
+# ** - степень
+
+# print(var + 7)
+# print(var - 7)
+# print(var * 7)
+# print(var / 7)
+# print((int)(var / 7))
+# print(var // 7)
+# print(var % 7)
+# print(var ** 7)
+
+# var = 120
+# result = var // 10
+# print(var, ',', result)
+# print(120, 12, sep=', ', end="!\n")
+# print(120, 12, sep=', ', end="!")
+
+# print('Введите число: ', end='')
+# F = int(input('Введите число: '))
+# # F = 250.01
+# C = (5 / 9) * (F - 32) 
+# print(f'{F}F = {C:.2f}C')
+
+
+# RUBLES = int(input('введите сумму в рублях: '))
+# DOLLAR = RUBLES / 100
+# print (f'{RUBLES} рублей это {DOLLAR} долларов')
+
+# if условие:
+#     pass
+# elif условие:
+#     pass
+# else:
